68c682d7deb08577c7e668a1cbd8187acf481f7f
ArianPaymozd <70002472+ArianPaymozd@users.noreply.github.com>
2021-04-01 21:18:16 -0700

    update cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(morgan(morganOption))
 app.use(helmet())
 
 const corsOptions = {
-  origin: config.CLIENT_ORIGIN,
+  origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 app.use(cors(corsOptions))
