635ecb64ef2da2e6d18bd0f74f5f80bfbbf74beb
ArianPaymozd <70002472+ArianPaymozd@users.noreply.github.com>
2021-04-02 16:32:48 -0700

    update postgrator

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   CLIENT_ORIGIN : 'http://localhost:3000',
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://arian:superoreo1@localhost/oppa',
+  DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
